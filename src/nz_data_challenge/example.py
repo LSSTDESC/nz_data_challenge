a0b3f04728b1383f31891cb5a0ec38e8a02262fe
Eric Charles <badass@stanford.edu>
2026-08-24 16:20:05 -0700

    updates to example

--- src/nz_data_challenge/example.py
+++ src/nz_data_challenge/example.py
@@ -9,6 +9,7 @@ from rail.estimation.algos.uniform_binning import UniformBinningClassifier
 
 from rail.core.data import TableHandle, QPHandle
 from rail.utils import catalog_utils
+import tables_io
 
 from .utils import TOMO_BIN_EDGES, TASKSETS, SIMS, SCENARIOS
 
@@ -16,8 +17,12 @@ from .utils import TOMO_BIN_EDGES, TASKSETS, SIMS, SCENARIOS
 # and a URL to download the sumission data files
 # and needed model files
 SUBMISSION_NAME: str = "example"
-SUBMISSION_URL: str = "http://s3df.slac.stanford.edu/people/echarles/data_challenge/nz_estimates_example.tgz"
-MODEL_URL: str = "http://s3df.slac.stanford.edu/people/echarles/data_challenge/nz_models_example.tgz"
+SUBMISSION_URL: str = (
+    "http://s3df.slac.stanford.edu/people/echarles/data_challenge/nz_estimates_example.tgz"
+)
+MODEL_URL: str = (
+    "http://s3df.slac.stanford.edu/people/echarles/data_challenge/nz_models_example.tgz"
+)
 
 
 # RAIL setup
@@ -30,7 +35,7 @@ catalog_utils.apply(CATALOG_TAG)
 def get_tomo_bin_edges(
     key: str,
 ) -> np.ndarray:
-    taskset = key[0:9]    
+    taskset = key[0:9]
     tomo_bin_edges = TOMO_BIN_EDGES[taskset]
     return tomo_bin_edges
 
@@ -41,9 +46,19 @@ def run_training(
     ddf_files: list[str | Path],
     models_dir: str | Path,
 ) -> None:
-    
-    model_file = f"{models_dir}/nz_challenge_{key}_pz_model.pkl"    
+
+    model_file = f"{models_dir}/nz_challenge_{key}_pz_model.pkl"
     ddf_file = ddf_files[0]
+
+    # Replace the np.nan redshifts with manyband redshfits and write a 'cleaned' file
+    ddf_file_cleaned = ddf_file.replace(".hdf5", "_cleaned.hdf5")
+    ddf_data = tables_io.read(ddf_file)
+    ddf_data["redshift"] = np.where(
+        np.isfinite(ddf_data["redshift"]),
+        ddf_data["redshift"],
+        ddf_data["redshift_manyband"],
+    )
+    tables_io.write(ddf_data, ddf_file_cleaned)
 
     # Make a KNN estiamtor and run it
     knn_informer = KNearNeighInformer.make_stage(
@@ -52,7 +67,7 @@ def run_training(
         hdf5_groupname="",
     )
 
-    test_handle = TableHandle(f'input_{key}', path=ddf_file)            
+    test_handle = TableHandle(f"input_{key}", path=ddf_file_cleaned)
     inform = knn_informer.inform(test_handle)
 
 
@@ -62,22 +77,22 @@ def run_estimation(
     models_dir: str | Path,
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path,    
+    output_nz_samples_file: str | Path,
 ) -> None:
-    
+
     tomo_bin_edges = get_tomo_bin_edges(key)
-    tomo_bin_centers = 0.5*(tomo_bin_edges[0:-1]+tomo_bin_edges[1:])
+    tomo_bin_centers = 0.5 * (tomo_bin_edges[0:-1] + tomo_bin_edges[1:])
     n_tomo_bins = len(tomo_bin_edges) - 1
 
     model_file = f"{models_dir}/nz_challenge_{key}_pz_model.pkl"
     pz_file = f"{models_dir}/nz_challenge_{key}_pz_estimates.hdf5"
-    
+
     # Make a KNN estiamtor and run it
     knn_estimate = KNearNeighEstimator.make_stage(
         name=f"estimate_{key}",
         model=model_file,
         hdf5_groupname="",
-        id_col='object_id',
+        id_col="object_id",
         nzbins=301,
         zmax=3.0,
         chunk_size=10000,
@@ -89,7 +104,7 @@ def run_estimation(
         name=f"classify_{key}",
         zbin_edges=tomo_bin_edges,
         no_assign=-1,
-        object_id_col='object_id',
+        object_id_col="object_id",
     )
 
     # Using naive pdf stacking to summarize the n(z) distritubions
@@ -104,35 +119,42 @@ def run_estimation(
     do_pz_estimate = True
     do_nz_bin_assignment = True
     do_nz_estimate = True
-    
+
     test_handle = TableHandle(f"test_{key}", path=str(wfd_file))
-    
+
     if do_pz_estimate:
         pz_estimates = knn_estimate.estimate(test_handle)
         # This is so that the next stage reads the whole file, not just the
-        # current chunk               
+        # current chunk
         pz_estimates.data = None
-        os.system(f"cp {pz_estimates.path} {pz_file}")  
+        os.system(f"cp {pz_estimates.path} {pz_file}")
     else:
-        pz_estimates = QPHandle(f'output_{key}', path=pz_file)
+        pz_estimates = QPHandle(f"output_{key}", path=pz_file)
 
     if do_nz_bin_assignment:
         bin_assignments = bin_classifier.classify(pz_estimates)
         # This is so that the next stage reads the whole file, not just the
         # current chunk
-        bin_assignments.data = None               
-        os.system(f"cp {bin_assignments.path} {output_bhat_file}")  
+        bin_assignments.data = None
+        os.system(f"cp {bin_assignments.path} {output_bhat_file}")
     else:
-        bin_assignments = TableHandle(f'bhat_{key}', path=output_bhat_file)
-                
+        bin_assignments = TableHandle(f"bhat_{key}", path=output_bhat_file)
+
     if do_nz_estimate:
-        samples_nz = summarizer.summarize(pz_estimates, bin_assignments)                
-        single_nz = summarizer.get_handle('single_NZ')
+        samples_nz = summarizer.summarize(pz_estimates, bin_assignments)
+        single_nz = summarizer.get_handle("single_NZ")
         os.system(f"cp {samples_nz.path} {output_nz_samples_file}")
         os.system(f"cp {single_nz.path} {output_nz_estimate_file}")
     else:
-        samples = QPHandle(f'output_summarize_{key}', path=output_nz_samples_file)
-        single_nz = QPHandle(f'single_NZ_summarize_{key}', path=output_nz_estimate_file)
+        samples = QPHandle(f"output_summarize_{key}", path=output_nz_samples_file)
+        single_nz = QPHandle(f"single_NZ_summarize_{key}", path=output_nz_estimate_file)
+
+
+def copy_samples_files(
+    input_nz_samples_file: str | Path,
+    output_nz_samples_file: str | Path,
+) -> None:
+    os.system(f"\\mv {input_nz_samples_file} {output_nz_samples_file}")
 
 
 def run_taskset_1_estimation_only(
@@ -141,12 +163,17 @@ def run_taskset_1_estimation_only(
     models_dir: str | Path,
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path | None,    
-) -> None:    
+    output_nz_samples_file: str | Path | None,
+) -> None:
     run_estimation(
-        key, wfd_file, models_dir,  output_nz_estimate_file, output_bhat_file, output_nz_samples_file,
+        key,
+        wfd_file,
+        models_dir,
+        output_nz_estimate_file,
+        output_bhat_file,
+        output_nz_samples_file,
     )
-    
+
 
 def run_taskset_2_estimation_only(
     key: str,
@@ -154,10 +181,15 @@ def run_taskset_2_estimation_only(
     models_dir: str | Path,
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path | None,    
+    output_nz_samples_file: str | Path | None,
 ) -> None:
     run_estimation(
-        key, wfd_file, models_dir,  output_nz_estimate_file, output_bhat_file, output_nz_samples_file,
+        key,
+        wfd_file,
+        models_dir,
+        output_nz_estimate_file,
+        output_bhat_file,
+        output_nz_samples_file,
     )
 
 
@@ -167,11 +199,21 @@ def run_taskset_3_estimation_only(
     models_dir: str | Path,
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path,    
+    output_nz_samples_file: str | Path,
 ) -> None:
-    run_estimation(
-        key, wfd_file, models_dir,  output_nz_estimate_file, output_bhat_file, output_nz_samples_file,
+    input_nz_samples_file = Path(
+        output_nz_samples_file.replace("taskset_3", "taskset_2")
     )
+    if not input_nz_samples_file.exists():
+        run_taskset_2_estimation_only(
+            key.replace("taskset_3", "taskset_2"),
+            wfd_file.replace("taskset_3", "taskset_2"),
+            models_dir,
+            output_nz_estimate_file.replace("taskset_3", "taskset_2"),
+            output_bhat_file.replace("taskset_3", "taskset_2"),
+            output_nz_samples_file.replace("taskset_3", "taskset_2"),
+        )
+    copy_samples_files(output_nz_samples_file)
 
 
 def run_taskset_1_training_and_estimation(
@@ -181,11 +223,16 @@ def run_taskset_1_training_and_estimation(
     ddf_files: list[str | Path],
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path | None,     
+    output_nz_samples_file: str | Path | None,
 ) -> None:
     run_training(key, wfd_file, ddf_files, models_dir)
     run_estimation(
-        key, wfd_file, models_dir,  output_nz_estimate_file, output_bhat_file, output_nz_samples_file,
+        key,
+        wfd_file,
+        models_dir,
+        output_nz_estimate_file,
+        output_bhat_file,
+        output_nz_samples_file,
     )
 
 
@@ -196,12 +243,16 @@ def run_taskset_2_training_and_estimation(
     ddf_files: list[str | Path],
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path | None,    
-    
+    output_nz_samples_file: str | Path | None,
 ) -> None:
     run_training(key, wfd_file, ddf_files, models_dir)
     run_estimation(
-        key, wfd_file, models_dir,  output_nz_estimate_file, output_bhat_file, output_nz_samples_file,
+        key,
+        wfd_file,
+        models_dir,
+        output_nz_estimate_file,
+        output_bhat_file,
+        output_nz_samples_file,
     )
 
 
@@ -212,10 +263,20 @@ def run_taskset_3_training_and_estimation(
     ddf_files: list[str | Path],
     output_nz_estimate_file: str | Path,
     output_bhat_file: str | Path,
-    output_nz_samples_file: str | Path,    
+    output_nz_samples_file: str | Path,
 ) -> None:
-    run_training(key, wfd_file, ddf_files, models_dir)
-    run_estimation(
-        key, wfd_file, models_dir,  output_nz_estimate_file, output_bhat_file, output_nz_samples_file,
-    )
 
+    input_nz_samples_file = Path(
+        output_nz_samples_file.replace("taskset_3", "taskset_2")
+    )
+    if not input_nz_samples_file.exists():
+        run_taskset_3_training_and_estimation(
+            key.replace("taskset_3", "taskset_2"),
+            wfd_file.replace("taskset_3", "taskset_2"),
+            models_dir,
+            [val.replace("taskset_3", "taskset_2") for val in ddf_files],
+            output_nz_estimate_file.replace("taskset_3", "taskset_2"),
+            output_bhat_file.replace("taskset_3", "taskset_2"),
+            output_nz_samples_file.replace("taskset_3", "taskset_2"),
+        )
+    copy_samples_files(output_nz_samples_file)
